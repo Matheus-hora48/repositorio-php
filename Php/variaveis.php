f73a96731c09b0db98741a69b274b8cbecd7ec7a
Matheus Hora <matheus11hora@gmail.com>
2022-02-16 21:51:02 -0300

    php

--- Php/variaveis.php
+++ Php/variaveis.php
@@ -16,20 +16,32 @@
   Sempre começar com $
 
   string: são as palavras, frases declaradas
-  int:
-  float
-  boolean:
+  int: valor inteiro
+  float valor real
+  boolean: true ou false
 
+  declara o tipo da varialvel sozinho
   */
 
   //string
-  $nome = 'Matheus Hora'
-
+  $nome = 'Matheus Hora';
+  //int
+  $idade = 20;
+  //float
+  $peso = 74.5;
+  //boolean
+  $fumante = true
 
   ?>
   <h1>Dados cadastrais</h1>
   <br />
-  <p>Ficha do cliente: <?php $nome ?></p>
+  <p>Ficha do cliente: <?= $nome ?></p>
+  <br />
+  <p>Idade do cliente: <?= $idade ?></p>
+  <br />
+  <p>Peso do cliente: <?= $peso ?></p>
+  <br />
+  <p>Ele é um fumante: <?= $fumante ?></p>
 
 
 </body>
