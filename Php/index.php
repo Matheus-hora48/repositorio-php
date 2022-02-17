c15b92eea35e2c1daef8418fd5ee4d68c83a187b
Matheus Hora <matheus11hora@gmail.com>
2022-02-16 21:43:29 -0300

    php

--- Php/index.php
+++ Php/index.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
   <!-- tag padrão php -->
   <?php
-    echo 'Utilizando tag padrão'
+  echo 'Utilizando tag '
   ?>
   <!-- tag de impressão -->
   <?=
-    'Utilizando tag padrão'
+  'Utilizando tag de impressão'
   ?>
+  <!-- tag curta php -->
+  <? echo 'Utilizando tag curta' ?>
 </body>
+
 </html>
---
+++ Php/variaveis.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+
+  /*
+  No php exite varios tipo de variaveis;
+  Sempre começar com $
+
+  string: são as palavras, frases declaradas
+  int:
+  float
+  boolean:
+
+  */
+
+  //string
+  $nome = 'Matheus Hora'
+
+
+  ?>
+  <h1>Dados cadastrais</h1>
+  <br />
+  <p>Ficha do cliente: <?php $nome ?></p>
+
+
+</body>
+
+</html>
