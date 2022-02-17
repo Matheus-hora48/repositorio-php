16405d615287dd90e97f5e40bb969df53352cdb2
Matheus Hora <matheus11hora@gmail.com>
2022-02-16 21:05:24 -0300

    Inicio no php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- tag padrão php -->
+  <?php
+    echo 'Utilizando tag padrão'
+  ?>
+  <!-- tag de impressão -->
+  <?=
+    'Utilizando tag padrão'
+  ?>
+</body>
+</html>
