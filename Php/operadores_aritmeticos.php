1b7bb5246afc521ae46c5efab551d03882f86815
Matheus Hora <matheus11hora@gmail.com>
2022-02-18 23:18:11 -0300

    Novos assuntos

---
+++ Php/operadores_aritmeticos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<bod>
+  <?php
+
+  // (+) faz a soma dos numeros
+  // (-) faz a subtração dos numero
+  // (*) faz a multiplicação
+  // (/) faz a divisão
+  // (%) Resto inteiro da divisão
+  $num1 = 13;
+  $num2 = 4;
+
+  echo "(+)".($num1+$num2);
+  echo '<br/>';
+  echo "(-)".($num1-$num2);
+  echo '<br/>';
+  echo "(*)".($num1*$num2);
+  echo '<br/>';
+  echo "(/)".($num1/$num2);
+  echo '<br/>';
+  echo "(%)".($num1%$num2);
+  echo '<br/>';
+  ?>
+</body>
+</html>
