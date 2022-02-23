7035862b2caff990f6d0e014b7d853665d48dfb3
Matheus Hora <matheus11hora@gmail.com>
2022-02-23 15:00:16 -0300

    Novo arquivos

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once('menu.php'); //quando nao acha ele produz um warning (apenas um alerta) 
+
+// com o once ele aceita apenas o primeiro script
+
+
+//require('menu.php'); //da um fatal error (interrompe o funcionamento do script)
+
+?>
+
+Pagina de inicio
+
+<?php
+
+include('menu.php'); //quando nao acha ele produz um warning (apenas um alerta)
+
+
+//require('menu.php'); //da um fatal error (interrompe o funcionamento do script)
+
+?>
